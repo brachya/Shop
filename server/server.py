04540eebec107545885cf531340780ec668396e6
Brachya <brachyaa@gmail.com>
2024-02-15 18:08:34 +0200

    server documentation added

--- server/server.py
+++ server/server.py
@@ -6,15 +6,21 @@ from tree import Tree, Node
 
 
 class ShopServer:
+    """
+    server for customer debts
+    you have to run instance.start_connection as main loop to receive
+    """
+
     clients: list[socket.socket] = []
-    IP = "127.0.0.1"
-    PORT = 34500
     MUTEX = Lock()
 
-    def __init__(self, file_path: str) -> None:
+    def __init__(self, file_path: str, ip: str, port: int) -> None:
+        self.ip = ip
+        self.port = port
+
         # starting server
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((self.IP, self.PORT))
+        self.server_socket.bind((self.ip, self.port))
         self.server_socket.listen(5)
         print("server starting...")
         self.file_path = file_path
@@ -37,6 +43,10 @@ class ShopServer:
         self.tree_to_tree()
 
     def validation(self, customer: str) -> list[str]:
+        """
+        get customer properties like [parameter, value] and check all values than
+        returns errors if exist or the value "true" in the first index and than all the details
+        """
         customer_details: list[list[str]] = [c.split("=") for c in customer.split(",")]
         details = [s[1] for s in customer_details]
         to_valid = Validate(details)
@@ -48,6 +58,10 @@ class ShopServer:
         return valid  # in case of errors
 
     def customer_exist_update(self, new_details: list[str], customer: Node) -> str:
+        """
+        get properties after identity check and than check if name and last name exist
+        and than updating the customer and adding line to csv file
+        """
         error = ""
         if new_details[0].capitalize() != customer.name:
             error += f"did you mean {customer.name}"
@@ -66,6 +80,10 @@ class ShopServer:
     def customer_update(
         self, new_details: list[str], customer: Node, start: bool = False
     ) -> None:
+        """
+        after customer exist and fit updating the Node of the customer
+        than replace the Node in the tree of dept and date
+        """
         customer.dept = customer.dept + int(new_details[5])
         customer.date = str(new_details[4])
         customer.vars["dept"] = customer.dept
@@ -81,12 +99,19 @@ class ShopServer:
             self.tree_by_date.add_node(customer)
 
     def add_line_to_file(self, customer_details: list[str]) -> None:
+        """
+        getting details of customer with no parameters and adding to the last line in the file
+        """
         with open(self.file_path, "a+", encoding="UTF-8") as fw:
             fw.write(
                 f"{customer_details[0].capitalize()},{customer_details[1].capitalize()},{int(customer_details[2])},{int(customer_details[3])},{customer_details[4]},{int(customer_details[5])}\n"
             )
 
     def set_new_customer(self, customer_details: list[str]) -> None:
+        """
+        ONLY NEW CUSTOMER
+        adding the details of the customers into all the trees
+        """
         customer = Node(
             customer_details[0].capitalize(),
             customer_details[1].capitalize(),
@@ -100,20 +125,23 @@ class ShopServer:
         self.add_line_to_file(customer_details)
 
     def start_connection(self) -> None:
-        try:
-            cli_sock, cli_add = self.server_socket.accept()
-        except ConnectionResetError:
-            quit()
-        except OSError:
-            quit()
-        else:
-            print(f"Client {cli_add} connected")
-            self.clients.append(cli_sock)
-            Thread(
-                target=self.connection_handle, args=(cli_sock, cli_add), daemon=True
-            ).start()
+        """the main running that open connection per client"""
+        while True:
+            try:
+                cli_sock, cli_add = self.server_socket.accept()
+            except ConnectionResetError:
+                break
+            except OSError:
+                break
+            else:
+                print(f"Client {cli_add} connected")
+                self.clients.append(cli_sock)
+                Thread(
+                    target=self.connection_handle, args=(cli_sock, cli_add), daemon=True
+                ).start()
 
     def csv_import(self, binary_tree: Tree, orderby: str) -> bool:
+        """when the server is start read from file the data into one tree"""
         print(f"csv importing to {orderby}")
         try:
             customers = self.open_file()
@@ -140,6 +168,9 @@ class ShopServer:
         return True
 
     def tree_to_tree(self) -> None:
+        """
+        calling csv by function and than create the ways of the trees into the nodes
+        """
         if not self.csv_import(self.tree_by_id, "identity"):
             print("No customers")
             return
@@ -152,6 +183,7 @@ class ShopServer:
                 self.trees[tree].add_node(node)
 
     def fix_file(self, all: list[str]) -> None:
+        """before reading the file remove all empty lines"""
         empty = 0
         for a in all:
             if a == "":
@@ -163,6 +195,7 @@ class ShopServer:
                 csv.write(a + "\n")
 
     def open_file(self) -> list[list[str]]:
+        """open the file and calling by instance.csv_import"""
         with open(self.file_path, "r", encoding="UTF-8") as csv:  # encoding for hebrew
             non_ordered_data: list[str] = csv.read().split("\n")
             if "" in non_ordered_data:
@@ -175,6 +208,9 @@ class ShopServer:
             return data
 
     def customer_str_to_list(self, customer: str) -> list[str]:
+        """
+        call by open_file and for reading the file and ordering the values for the nodes
+        """
         customer_lst: list[str] = customer.split(",")
         fn: str = customer_lst[0]
         ln: str = customer_lst[1]
@@ -185,6 +221,7 @@ class ShopServer:
         return [fn, ln, identity, phn, dt, dp]
 
     def zero_add(self, val: str, length: int) -> str:
+        """filling the value with zero from the left"""
         while len(val) != length:
             val = "0" + val
         return val
@@ -202,20 +239,24 @@ class ShopServer:
         return None
 
     def trimer(self, word: str) -> str:
+        """trim white space from left and right and also return words with one space"""
         return " ".join(word.split())
 
     def send_to_client(self, client_socket: socket.socket, message: str) -> None:
+        """used to send message and than string to tell the client to stop"""
         client_socket.sendall(message.encode())
         client_socket.sendall("&%^$*$(#)@!".encode())
 
     def connection_handle(
         self, client_socket: socket.socket, client_address: tuple[str, int]
     ) -> None:
+        """for each customer it is the main server"""
         while True:
             try:
                 message = client_socket.recv(1024).decode("UTF-8")
             except ConnectionResetError:
                 print(f"{client_address} shuted off")
+                self.clients.remove(client_socket)
                 break
 
             if message.startswith("set"):
@@ -265,8 +306,11 @@ class ShopServer:
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        shop = ShopServer(sys.argv[1])
+        shop = ShopServer(sys.argv[1], "127.0.0.1", 34500)
     else:
-        shop = ShopServer("C:\\Studies\\Final_Project\\server\\db.csv")
-    while True:
-        shop.start_connection()
+        shop = ShopServer(
+            "C:\\Studies\\Final_Project\\server\\db.csv", "127.0.0.1", 34500
+        )
+    t = Thread(target=shop.start_connection)
+    t.start()
+    t.join()
